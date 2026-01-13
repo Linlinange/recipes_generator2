c31fbd9e3848e189a9c909e141c9f508e9bb26db
Linlinange <linlinange@163.com>
2026-01-13 16:54:21 +0800

    初始化

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+MC Recipe Generator - 核心模块
+
+这个包包含了配方生成器的所有核心功能类：
+- Config: 配置管理
+- Template: 模板处理
+- ReplacementEngine: 替换引擎
+- RecipeGenerator: 主生成器
+
+所有类都遵循单一职责原则，便于测试和维护。
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
